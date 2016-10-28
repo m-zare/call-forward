af25297bcf3d4b7604500c03dfb2d90825d47008
Mostafa Zare <mostafazare@gmail.com>
2016-10-28 21:05:14 +0330

    First structure

--- src/asterisk/Socket.java
+++ src/asterisk/Socket.java
@@ -1,7 +1,33 @@
 package asterisk;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by Mostafa on 10/28/2016.
  */
 public class Socket {
+    protected String ip;
+    protected int port;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        if (PATTERN.matcher(ip).matches()){
+            this.ip = ip;
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        if (port > 0 && port <= 65536){
+            this.port = port;
+        }
+    }
+
+    private Pattern PATTERN = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
 }
---
+++ src/client/Main.java
@@ -0,0 +1,7 @@
+package client;
+
+/**
+ * Created by Mostafa on 10/28/2016.
+ */
+public class Main {
+}
