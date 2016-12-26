9e7e2c9e32f2c3cb4ee7ef739546156e6d595a5e
Mostafa Zare <mostafazare@gmail.com>
2016-12-26 18:50:31 +0330

    add astdb actions.

--- src/model/da/EventHandler.java
+++ src/model/da/EventHandler.java
@@ -6,11 +6,12 @@ import model.entity.*;
  * Created by Mostafa on 11/18/2016.
  */
 public interface EventHandler {
-    public void call(EventEntity e, ConnectionEntity connection);
+    void call(EventEntity e, ConnectionEntity connection);
 
-    public void callerID(EventEntity e, ConnectionEntity connection);
+    void callerID(EventEntity e, ConnectionEntity connection);
 
-    public void hangUp(EventEntity e, ConnectionEntity connection);
+    void hangUp(EventEntity e, ConnectionEntity connection);
 
-    public void asyncAgi(EventEntity e, ConnectionEntity connection);
+    void asyncAgi(EventEntity e, ConnectionEntity connection);
+
 }
